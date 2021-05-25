253e84de9bd7bc9a34af3c10952b00c4e7a038a7
homoant <77457836+homoant@users.noreply.github.com>
2021-05-25 14:01:06 -0400

    Create  Take input adjacency matrix.cpp

---
+++ Take input adjacency matrix.cpp
@@ -0,0 +1,25 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+const int mx = 1e3+123;
+int adjMat[mx][mx];
+
+int main()
+{
+    optimize();
+    
+    int n;
+    cin >> n;
+    
+    for ( int i = 1; i <= n; i++ ) {
+        for ( int j = 1; j <= n; j++ ) cin >> adjMat[i][j];
+    }
+
+
+    return 0;
+}
